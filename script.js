b62ebfb75285692c0f89cb76556a36117f8d0e2e
Vin <110591050+vincingdi@users.noreply.github.com>
2025-08-01 19:41:53 +0300

    Refactor display population and script loading for improved functionality

--- script.js
+++ script.js
@@ -14,22 +14,18 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(add(4, 3));
-console.log(subtract(4, 3));
-console.log(multiply(4, 4));
-console.log(divide(4, 4));
 
 let num1, num2, operator;
 
 function populateDisplay() {
-    buttons = document.querySelectorAll(".populate");
+    const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            buttonText = button.textContent;
-            console.log(buttonText);
+            let buttonText = button.innerHTML;
+            const display = document.getElementById("display");
+            display.textContent += buttonText;
         });
     });
-
 
 }
 
