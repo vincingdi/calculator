9de9decbed82ec7821640d954a1cd809efb912d4
Vin <110591050+vincingdi@users.noreply.github.com>
2025-08-03 10:34:31 +0300

    Enhance calculator functionality with display updates and operator handling

--- script.js
+++ script.js
@@ -18,12 +18,34 @@ function divide(a, b) {
 let num1, num2, operator;
 
 function populateDisplay() {
+    let count = 0;
+    const display = document.getElementById("display");  
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             let buttonText = button.innerHTML;
-            const display = document.getElementById("display");
-            display.textContent += buttonText;
+            display.textContent += " " + buttonText + " ";
+            count++;
+            switch (count) {
+                case 1: 
+                    num1 = buttonText;
+                    console.log("Hello");
+                    break;
+                case 2:
+                    operator = buttonText;
+                    break;
+                case 3:
+                    num2 = buttonText;
+                    break;
+                case 4:
+                    operate(num1, num2, operator);
+                default:
+            }
+            console.log(count);
+            console.log(num1, num2, operator);
+            if (buttonText === "=") {
+                operate(num1, num2, operator);
+            }
         });
     });
 
@@ -32,6 +54,16 @@ function populateDisplay() {
 populateDisplay();
 
 function operate(num1, num2, operator) {
+    const display = document.getElementById("display");
+    if (operator === "+") {
+        display.textContent = add(Number(num1), Number(num2));
+    } else if (operator === "-") {
+        display.textContent = subtract(Number(num1), Number(num2));
+    } else if (operator === "*") {
+        display.textContent = multiply(Number(num1), Number(num2));
+    } else if (operator === "/") {
+        display.textContent = divide(Number(num1), Number(num2));
+    }
 
     return 0;
 }
